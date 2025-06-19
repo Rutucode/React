13ca51580930188ea8c866dee4b2c51348800f6a
Rutuja <rutujapawarhere08@gmail.com>
2025-06-19 07:48:28 +0530

    practice

--- selfPractice_Trial/index.jsx
+++ selfPractice_Trial/index.jsx
@@ -1,20 +1,24 @@
-import { createRoot } from "react-dom/client";
-import { createElement } from "react";
+// import { createRoot } from "react-dom/client";
+// import { createElement } from "react";
 
-// Create Root 
-const root = createRoot(document.getElementById('#root'));
-//Reder markup
-//root.render(<h1>Self React Practice </h1>)
-const reactElement = createElement("h1", null, "Hello form createElement!")
-console.log(reactElement)
-root.render(reactElement
-)
+// // Create Root 
+// const root = createRoot(document.getElementById('#root'));
+// //Reder markup
+// //root.render(<h1>Self React Practice </h1>)
+// const reactElement = createElement("h1", null, "Hello form createElement!")
+// console.log(reactElement)
+// root.render(reactElement
+// )
 
 
-createRoot(document.getElementById("root-two")).render(
-    <ul>
-        <li>Super popular JS library</li>
-        <li>Will help me be even more employable</li>
-        <li>React has a pretty cool logo</li>
-    </ul>
-)
+// createRoot(document.getElementById("root-two")).render(
+//     <ul>
+//         <li>Super popular JS library</li>
+//         <li>Will help me be even more employable</li>
+//         <li>React has a pretty cool logo</li>
+//     </ul>
+// )
+
+const heading = React.createElement("h1", {}, "Hello from React!", [React.createElement("div", {id: child}, "Child in react")])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.Render(heading)
