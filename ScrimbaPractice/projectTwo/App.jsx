e7569cd1314c6448cda50bd69cad47f6e106cc6a
Rutuja <rutujapawarhere08@gmail.com>
2025-06-20 10:06:39 +0530

    scrimba practice code

---
+++ ScrimbaPractice/projectTwo/App.jsx
@@ -0,0 +1,16 @@
+import Jokes from "Jokes"
+
+function App(){
+    return(
+        <>
+        <Jokes 
+            Setup = "How did the hacker escape the police?"
+            punchline = "He just ransomware!"
+            upvotes = {10}
+            isPun = {true}
+        />
+        </>
+    )
+}
+
+
---
+++ ScrimbaPractice/projectTwo/Jokes.jsx
@@ -0,0 +1,38 @@
+export default function Jokes(props){
+    return(
+        <>
+        <p>{props.Setup}</p>
+        <p>{props.punchline}</p>
+        </>
+    )
+}
+/*
+export default function App() {
+    return (
+        <main>
+            <Joke
+                punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
+                upvotes={10}
+                isPun={true}
+            />
+            <Joke
+                setup="How did the hacker escape the police?"
+                punchline="He just ransomware!"
+            />
+            <Joke
+                setup="Why don't pirates travel on mountain roads?"
+                punchline="Scurvy."
+            />
+            <Joke
+                setup="Why do bees stay in the hive in the winter?"
+                punchline="Swarm."
+            />
+            <Joke
+                setup="What's the best thing about Switzerland?"
+                punchline="I don't know, but the flag is a big plus!"
+            />
+        </main>
+    )
+}
+
+*/
